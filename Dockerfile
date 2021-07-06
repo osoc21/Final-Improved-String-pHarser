4bfc33bd9bc64082867ccd369dddda3b74ff1c0e
Denperidge <denperidge@gmail.com>
2021-07-06 15:34:39 +0200

    Updated docker-tester, commented it in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ COPY app/ .
 
 RUN pip3 install -r requirements.txt
 
-CMD ["python3", "./docker-tester.py"]
+# CMD ["python3", "./docker-tester.py"]
--- app/docker-tester.py
+++ app/docker-tester.py
@@ -6,7 +6,7 @@ import os
     docker run fish python3 docker-tester.py
 """
 
-"""This print should return 200. This will prove that the following work:
+"""This print should return 200. This will prove that the following things work:
     - Internet connectivity through a google request
     - HTTPS requests
     - Installing python requirements through pip
@@ -14,9 +14,9 @@ import os
 """
 print(requests.get("https://www.google.com").status_code)
 
-"""This print should return the version of anystyle-cli. This will prove that the following work:
+"""This print should return the help of anystyle-cli. This will prove that the following things work:
     - The ruby installation
     - The anystyle-cli installation
     - The connectivity between python and anystyle-cli
 """
-print(os.system("anystyle-cli --version"))
+print(os.system("anystyle"))
