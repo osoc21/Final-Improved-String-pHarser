5d1150f9a245a10c7131e6ef0df66dab4963c3bc
Felix Cammaerts <felix.cammaerts@hotmail.com>
2021-07-12 11:09:25 +0200

    train year models for each decade

---
+++ model/train_year_models.sh
@@ -0,0 +1,11 @@
+python3 train_and_accuracy.py examples-2251.csv 0.7 --end 1850 --out pre1850.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1851 --end 1900 --out 1851-1900.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1901 --end 1925 --out 1901-1925.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1926 --end 1950 --out 1926-1950.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1951 --end 1960 --out 1951-1960.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1961 --end 1970 --out 1961-1970.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1971 --end 1980 --out 1971-1980.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1981 --end 1990 --out 1981-1990.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 1991 --end 2000 --out 1991-2000.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 2001 --end 2010 --out 2001-2010.mod
+python3 train_and_accuracy.py examples-2251.csv 0.7 --start 2011 --end 2021 --out 2011-2021.mod
