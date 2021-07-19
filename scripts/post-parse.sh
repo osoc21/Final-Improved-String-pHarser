2ca7c912067d175734767c81cfd1155befdb71e8
Denperidge <denperidge@gmail.com>
2021-07-19 16:31:06 +0200

    Fixed post-parse script

--- scripts/post-parse.sh
+++ scripts/post-parse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Small shell script to test /parse
-curl -X POST -H "Content-Type: text/plain" -d @examplesToParse.txt http://localhost:5000
+curl -X POST -H "Content-Type: text/plain" -d @../model/data/csv/examplesToParse.txt http://localhost:5000
